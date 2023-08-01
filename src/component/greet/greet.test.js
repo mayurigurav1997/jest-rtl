2f459567a4bea566efbf1ca8483ac29904113300
Mayuri Gurav <mayurigurav1997@gmail.com>
2023-08-01 12:04:34 +0530

    describe for grouping

--- src/component/greet/greet.test.js
+++ src/component/greet/greet.test.js
@@ -1,16 +1,22 @@
 import { render, screen } from "@testing-library/react"
 import { Greet } from "./greet"
 
-test('greet render correctly', () => {
-    render(<Greet />)
-    const textElement = screen.getByText(/Hello/)
-    expect(textElement).toBeInTheDocument()
-})
+describe("Greet", () => {
+    test('render correctly', () => {
+        render(<Greet />)
+        const textElement = screen.getByText(/Hello/)
+        expect(textElement).toBeInTheDocument()
+    })
 
-test.only("great render with name", () => {
-    render(<Greet name="Mayuri" />)
-    const textElement = screen.getByText("Hello Mayuri")
-    expect(textElement).toBeInTheDocument()
+    test("render with name", () => {
+        render(<Greet name="Mayuri" />)
+        const textElement = screen.getByText("Hello Mayuri")
+        expect(textElement).toBeInTheDocument()
+    })
+    //test.skip - skip that test case from the file
+    //test.only - test only that test case from the file
 })
-//test.skip
-//test.only
+//describe is used for grouping
+//describe.only
+//describe.skip
+//we can use multipe describe and nested describe
