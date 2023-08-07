bff6ff2068f9a33f104b535cbe5e4cec2b727817
Mayuri Gurav <mayurigurav1997@gmail.com>
2023-08-07 13:29:27 +0530

    wrapper for themeing testing

--- src/component/mui/MuiMode.test.tsx
+++ src/component/mui/MuiMode.test.tsx
@@ -1,3 +1,20 @@
 import React from "react";
-import {render,screen} from"@testing-library/react""
-import {MuiMode} from './mui-mode'
+import { render, screen } from "@testing-library/react";
+import { MuiMode } from "./MuiMode";
+import { AppProviders } from "../../providers/AppProviders";
+
+describe("MuiMode", () => {
+  test("renders text correctly", () => {
+    render(<MuiMode />, {
+      wrapper: AppProviders,
+    });
+    const headingElement = screen.getByRole("heading");
+    expect(headingElement).toHaveTextContent("dark mode");
+  });
+
+  test("renders text in white color for dark mode", () => {
+    render(<MuiMode />);
+    const headingElement = screen.getByRole("heading");
+    expect(headingElement).toHaveStyle({ color: "'rgb(255, 255, 255)'" });
+  });
+});
